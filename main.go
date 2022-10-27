93486e558a1bf1c85d915fa14dee2556859a6e8e
Martin Sooväli <msoovali@gmail.com>
2022-10-27 11:45:23 +0300

    Fix archiver init

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/msoovali/aa-settlements/adapter/archiver"
 	"github.com/msoovali/aa-settlements/adapter/googledrive"
 	"github.com/msoovali/aa-settlements/adapter/googlesheet"
 	"github.com/msoovali/aa-settlements/adapter/translations"
 	"github.com/msoovali/aa-settlements/config"
 	"github.com/msoovali/aa-settlements/domain"
 	"github.com/msoovali/aa-settlements/domain/clock"
+	"github.com/msoovali/aa-settlements/domain/localizer"
 	"github.com/msoovali/aa-settlements/domain/port"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -36,13 +38,9 @@ func main() {
 	}
 	googleApiClient := googleApiConfig.Client(ctx)
 
-	ports, err := createPorts(googleApiClient, conf)
+	ports, localizer, err := createPortsAndLocalizer(googleApiClient, conf)
 	if err != nil {
 		log.Fatalf("Failed to create ports: %v", err)
-	}
-	localizer, err := ports.TranslationsPort.Get(conf.Locale)
-	if err != nil {
-		log.Fatalf("Failed to load locale: %v", err)
 	}
 	service, err := domain.New(ports, conf, clock.RealClock{}, localizer)
 	if err != nil {
@@ -56,22 +54,25 @@ func main() {
 	log.Println("Next month settlements prepared successfully!")
 }
 
-func createPorts(googleApiClient *http.Client, config *config.Config) (*port.Ports, error) {
+func createPortsAndLocalizer(googleApiClient *http.Client, config *config.Config) (*port.Ports, *localizer.Localizer, error) {
 	driveSrv, err := drive.NewService(context.Background(), option.WithHTTPClient(googleApiClient))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	driveService := googledrive.NewDriveGateway(driveSrv, config.RootFolderId)
 	sheetSrv, err := sheets.NewService(context.Background(), option.WithHTTPClient(googleApiClient))
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	sheetService := googlesheet.NewSheetGateway(sheetSrv)
 	translationsService := translations.NewTranslationsGateway()
+	localizer, err := translationsService.Get(config.Locale)
+	archiverService := archiver.New(driveService, *config, localizer)
 
 	return &port.Ports{
 		DrivePort:        driveService,
 		SheetPort:        sheetService,
 		TranslationsPort: translationsService,
-	}, nil
+		ArchiverPort:     archiverService,
+	}, localizer, err
 }
